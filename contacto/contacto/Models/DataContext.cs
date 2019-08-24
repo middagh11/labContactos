1f1b71f2d2359a013bb892410c081c0d2374a31b
leonardomiddagh@gmail.com <leonardomiddagh@gmail.com>
2019-08-23 21:01:25 -0400

    con entity framework

---
+++ contacto/contacto/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace contacto.Models
+
+{
+    using System.Data.Entity;
+
+    public class DataContext: DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+    }
+}
